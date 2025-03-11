721e18b24423675bb7e5f7f22e43bcd3ba9468c7
lupinenronja <ronja.scharnagl@iu-study.org>
2025-03-11 13:37:49 +0100

    Header für große Geräte gut

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tabelleFiltern () {
-    //Nutzerdaten aus DropDown holen, value lesen, Groß- und Kleinschreibung ignorieren
+    //Nutzerdaten aus DropDown holen, value lesen, Großschreibung ignorieren
     let nutzereingabeNation = document.getElementById("nation").value.toLowerCase();
     let nutzeringabeUnternehmen = document.getElementById("unternehmen").value.toLowerCase();
 
@@ -10,13 +10,13 @@ function tabelleFiltern () {
     //alle Tabellenzeilen prüfen, ob gleich Nutzeringabe
     for (let i = 1 ; i < zeilen.length; i++) { //thead überspringen, restliche Tabelle prüfen
         let spalten = zeilen[i].getElementsByTagName("td"); //Array mit Spaltenelementen
-
+        /*Erste Spalte (Unternehmen) durchsuchen, dabei Leerzeichen und Großschreibung 
+        ignorieren*/
         let unternehmen = spalten[0].innerText.trim().toLowerCase();  
-            //Erste Spalte (Unternehmen) durchsuchen, dabei Leerzeichen und Großschreibung 
-            //ignorieren
+        /*Zweite Spalte (Nation) durchsuchen, dabei Leerzeichen und Großschreibung 
+        ignorieren*/
         let nation = spalten[1].innerText.trim().toLowerCase();  
-            //Zweite Spalte (Nation) durchsuchen, dabei Leerzeichen und Großschreibung 
-            //ignorieren
+
 
         if ((nutzereingabeNation === "blanko" || nutzereingabeNation === nation) &&
         (nutzeringabeUnternehmen === "blanko" || nutzeringabeUnternehmen === unternehmen)) {
